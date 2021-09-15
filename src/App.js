8b6ed8d5c234d67c2634d26fcdfde0bcbf60c5f5
Ivan Rowlands de Macedo <ivan.jump@globo.com>
2021-09-15 00:53:08 -0300

    form with terms, button functional, seven validation, password verification has a bug that dont need to match to be accepted

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import "./App.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+function App() {
+  const formShema = yup.object().shape({
+    name: yup.string().required("nome obrigatorio"),
+    email: yup.string().required("email obrigatorio").email("email invalido"),
+    telephone: yup
+      .string()
+      .required("telefone obigatorio")
+      .matches("^[0-9]{2} ([0-9]{9})", "telefone invalido ddd 955555555"),
+    country: yup.string(),
+    birthDate: yup
+      .string()
+      .required("sua data de nacimento")
+      .matches(
+        "^[0-9]{2}/([0-9]{2}/[0-9]{2})",
+        "cada valor tem que ir separado com um '/'"
+      ),
+    password: yup
+      .string()
+      .required("senha obrigatoria")
+      .matches(
+        "",
+        "sua senha tem que incluir uma letra mayuscula, uma letra minuscula, um numero, e um caracter especial"
+      ),
+    confPassword: yup.string().required("senha tem que ser igual").matches(),
+    acept: yup
+      .string()
+      .required("click the box")
+      .matches(true, "tem que dar click na box"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formShema),
+  });
+
+  const onSubmitFunction = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="container">
+      <h3>formulario</h3>
+      <form className="form" onSubmit={handleSubmit(onSubmitFunction)}>
+        <input placeholder="Nome de usuario*" {...register("name")} />
+        {errors.name?.message}
+        <input placeholder="Email*" {...register("email")} />
+        {errors.email?.message}
+        <input placeholder="Telephone*" {...register("telephone")} />
+        {errors.telephone?.message}
+        <input placeholder="Country" {...register("country")} />
+        {errors.country?.message}
+        <input placeholder="dia mes ano" {...register("birthDate")} />
+        {errors.birthDate?.message}
+        <input type="password" placeholder="senha" {...register("password")} />
+        {errors.password?.message}
+        <input
+          type="password"
+          placeholder="confirmar senha"
+          {...register("confPassword")}
+        />
+        {errors.confPassword?.message}
+        <input type="checkbox" {...register("acept")} />
+        <p>aceita os terminos</p>
+        {errors.acept?.message}
+        <div className="box-1">
+          <button className="btn btn-one" type="submit">
+            <span>HOVER ME</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
